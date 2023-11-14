a4c008cf2761113eb51e1ddec6009a62323b1012
jdubrey <132595743+jdubrey@users.noreply.github.com>
2023-11-14 16:05:35 -0500

    Add ConvertTemp.js from CodeSandbox

---
+++ src/ConvertTemp.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export default function ConvertTemp() {
+  let [unit, setUnit] = useState("fahrenheit");
+  const [temperature, setTemperature] = useState(null);
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  if (unit === "fahrenheit") {
+    let celsius = (temperature - 32) * (5 / 9);
+    return (
+      <div className="ConvertTemp">
+        <span className="Daily">{Math.round(celsius)} </span>
+        <span className="Convert" onClick={showCelsius}>
+          °F| <a href="/"> °C </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="ConvertTemp">
+        <span className="Daily">{Math.round(temperature)} </span>
+        <span className="Convert" onClick={showFahrenheit}>
+          <a href="/"> °F </a>| °C
+        </span>
+      </div>
+    );
+  }
+}
