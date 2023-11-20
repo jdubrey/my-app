b72890fb2e31332fce0cbec15f1e210dfbe7c324
jdubrey <132595743+jdubrey@users.noreply.github.com>
2023-11-20 17:35:15 -0500

    Import CSS

--- src/WeeklyForecast.js
+++ src/WeeklyForecast.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./styles.css";
 import DailyForecast from "./DailyForecast";
 
 export default function WeeklyForecast(props) {
